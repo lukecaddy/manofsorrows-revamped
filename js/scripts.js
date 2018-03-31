304d476fe073b6fd232eeeb0193a306d61aec968
Luke_Caddy <lukecaddy@gmail.com>
2018-03-31 10:04:29 -0500

    Final
    -Updated design
    -Added rest of the posts
    -Combined About and Contact section

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// Navbar 
+
+$(document).ready(function() {
+  $('.drawer').drawer();
+}); 
+
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1100);
+    }
+});
+
+		
